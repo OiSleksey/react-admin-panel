b89795bde950106791302cf1eee0191b2801e333
OiSleksey <116756218+OiSleksey@users.noreply.github.com>
2023-06-27 19:32:27 +0300

    [Task40] Added animation in progress

--- src/components/ui/Progress/Progress.jsx
+++ src/components/ui/Progress/Progress.jsx
@@ -3,13 +3,23 @@ import Box from '@mui/material/Box';
 import LinearProgress from '@mui/material/LinearProgress';
 import { connect } from 'react-redux';
 import { getIsProgress } from '../../../store/selectors/ui.selector';
+import { motion, AnimatePresence } from 'framer-motion';
 
 const Progress = ({ isProgress }) => {
   console.log(isProgress);
   return (
-    <Box sx={{ width: '100%', opacity: isProgress ? 1 : 0 }}>
-      <LinearProgress />
-    </Box>
+    <motion.div
+      animate={{
+        opacity: isProgress ? 1 : 0,
+      }}
+      transition={{
+        duration: 1,
+      }}
+    >
+      <Box sx={{ width: '100%' }}>
+        <LinearProgress />
+      </Box>
+    </motion.div>
   );
 };
 
